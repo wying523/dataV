7901df454150ccb0fca10a45514afccfebd72ab9
Wang <wang_y@FEV.COM>
2017-11-30 16:52:52 -0500

    completed code_latency for hug data

--- EWMA_Filter.py
+++ EWMA_Filter.py
@@ -15,3 +15,18 @@ Length = len(Height)
 
 RatioMean = ratio.mean().values
 ratio_std = np.std(ratio.values)
+
+#EWMA coefficient
+Alpha = 0.1
+
+#initialize the filtered data frame
+S = pd.DataFrame(index = range(0,Length-1),columns = [1])
+
+
+temp = ratio.values[0][0]
+S.at[0,1] = temp
+
+for i in xrange(1,Length):
+    S.at[i,1] = (1-Alpha) * S.values[i-1,0] + Alpha * ratio.values[i,0]
+
+New_std = np.std(S.values)
